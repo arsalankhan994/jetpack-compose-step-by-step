d28d3d8254c8670dccc5a00798cb64ac560d9654
arsalankhan <arsalankhan994@gmail.com>
2022-03-19 13:37:09 +0500

    setting project

---
+++ app/src/main/java/com/erselankhan/compose/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.erselankhan.compose
+
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.erselankhan.compose.ui.theme.ComposeTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val context: Context = this
+
+    @OptIn(ExperimentalMaterialApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
